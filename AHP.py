22dd457d6fa4ef6a63a34824b1e3d7c2d700b150
syzab <48263677+syzab@users.noreply.github.com>
2020-05-05 00:11:37 +0200

    Update AHP.py

--- AHP.py
+++ AHP.py
@@ -8,20 +8,16 @@ def ahp(kategorie):
         for j in range(k_macierz.shape[0]):
             if i == j :
                 k_macierz[i][j] = 1
-
             if i < j:
                 inp = input(kategorie[i]+' do '+kategorie[j]+': ')
-
                 if inp == '>':
                     inp = 10
                 elif inp == '<':
                     inp = 0.1
                 else:
                     inp = 1
-
                 k_macierz[i][j] = inp
                 k_macierz[j][i] = k_macierz[i][j]**(-1)
-
 
     k_macierz = np.true_divide(k_macierz, np.sum(k_macierz, axis = 0)) #podziel każdy element kolumny przez sumę kolumny
     k_macierz = np.sum(k_macierz, axis = 1) / k_macierz.shape[0]       #zsumuj rzędy i podziel każdy przez liczbę kategorii
@@ -33,10 +29,7 @@ def ahp(kategorie):
     return kategorie_dict
 
 
-
 print('- określ preferencje jednej kategorii względem drugiej (>, =, <) - ')
 wynik = ahp(kategorie)
-
-
 print('-'*100+'\n',wynik,'\n'+'-'*100)
 
