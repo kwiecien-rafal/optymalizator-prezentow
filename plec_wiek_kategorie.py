990c0231f41f7cc6710e00a609da5a7ea60200ff
Rafał Kwiecień <kwiecien.rafal@gmail.com>
2020-05-06 17:53:32 +0200

    add ograniczanie bazy plcia i wiekiem

---
+++ plec_wiek_kategorie.py
@@ -0,0 +1,165 @@
+# ograniczenia na płeć
+kobiece_podkategorie = [
+    'agd małe',
+    'ogród',
+    'utrzymanie czystości',
+    'dla mamy i dziecka',
+    'pielegnacja',
+    'manicure i pedicure',
+    'makijaż',
+    'perfumy',
+]
+
+meskie_podkategorie = [
+    'komputery',
+    'konsole i automaty',
+    'narzędzia',
+    'budownictwo',
+    'perfumy',
+    'gry',
+    'wędkarstwo',
+    'militaria',
+    'części samochodowe',
+    'akcesoria samochodowe',
+    'opony i felgi',
+    'warsztat',
+    'do motocykli',
+    'chemia',
+    'części do maszyn'
+]
+
+kobiece_kategorie_szczegolowe = [
+    'zapachy dla kobiet'
+]
+
+meskie_kategorie_szczegolowe = [
+    'zapachy dla mężczyzn'
+]
+
+# ---------------------------------------------------------------
+# ograniczenia na wiek
+dziecko_podkategorie = [
+    'konsole i automaty',
+    'zabawki',
+    'zabawki ogrodowe'
+    'książki i komiksy',
+    'gry',
+    'instrumenty',
+    'kody i doładowania',
+    'muzyka',
+    'filmy',
+    'rowery i akcesoria',
+    'skating, slackline'
+]
+
+mlodziez_podkategorie = [
+    'telefony i akcesoria',
+    'komputery',
+    'konsole i automaty',
+    'fotografia',
+    'zabawki',
+    'zabawki ogrodowe',
+    'pielęgnacja',
+    'manicure i pedicure',
+    'makijaż',
+    'perfumy',
+    'książki i komiksy',
+    'gry',
+    'instrumenty',
+    'kody i doładowania',
+    'muzyka',
+    'filmy',
+    'rowery i akcesoria',
+    'turystyka',
+    'siłownia i fitness',
+    'wędkarstwo',
+    'skating, slackline',
+    'militaria'
+]
+
+dorosly_podkategorie = [
+    'telefony i akcesoria',
+    'komputery',
+    'telewizory i akcesoria',
+    'konsole i automaty',
+    'agd małe',
+    'agd',
+    'agb do zabudowy',
+    'fotografia',
+    'wyposażenie',
+    'narzędzia',
+    'oświetlenie',
+    'meble',
+    'budownictwo',
+    'ogród',
+    'produkty spożywcze',
+    'artykuły dla zwierząt',
+    'utrzymanie czystości',
+    'popularne',
+    'zdrowie i higiena',
+    'wózki dziecięce',
+    'dla mamy i dziecka',
+    'karmienie dziecka',
+    'pielęgnacja',
+    'manicure i pedicure',
+    'makijaż',
+    'perfumy',
+    'książki i komiksy',
+    'gry',
+    'instrumenty',
+    'kody i doładowania',
+    'muzyka',
+    'filmy',
+    'rowery i akcesoria',
+    'turystyka',
+    'siłownia i fitness',
+    'wędkarstwo',
+    'skating, slackline',
+    'militaria',
+    'części samochodowe',
+    'akcesoria samochodowe',
+    'opony i felgi',
+    'warsztat',
+    'do motocykli',
+    'chemia',
+    'części do maszyn'
+]
+
+starsza_podkategorie = [
+    'telewizory i akcesoria',
+    'agd małe',
+    'agd',
+    'agb do zabudowy',
+    'fotografia',
+    'wyposażenie',
+    'narzędzia',
+    'oświetlenie',
+    'meble',
+    'budownictwo',
+    'ogród',
+    'produkty spożywcze',
+    'artykuły dla zwierząt',
+    'utrzymanie czystości',
+    'popularne',
+    'pielęgnacja',
+    'manicure i pedicure',
+    'makijaż',
+    'perfumy',
+    'książki i komiksy',
+    'gry',
+    'instrumenty',
+    'muzyka',
+    'filmy',
+    'rowery i akcesoria',
+    'turystyka',
+    'siłownia i fitness',
+    'wędkarstwo',
+    'militaria',
+    'części samochodowe',
+    'akcesoria samochodowe',
+    'opony i felgi',
+    'warsztat',
+    'do motocykli',
+    'chemia',
+    'części do maszyn'
+]
