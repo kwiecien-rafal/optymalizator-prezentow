69c861a08e57bad10be63a80fba9ce77fa9fcb65
Musiall <48292927+Musiall@users.noreply.github.com>
2020-05-31 18:32:05 +0200

    Add files via upload

---
+++ okazje.py
@@ -0,0 +1,110 @@
+# ograniczenia na okazje
+urodziny_gwiazdka = [
+    'telefony i akcesoria',
+    'komputery',
+    'agd małe',
+    'fotografia',
+    'wyposazenie',
+    'manicure i pedicure',
+    'makijaż',
+    'perfumy',
+    'zabawki',
+    'ksiazki i komiksy',
+    'gry',
+    'instrumenty',
+    'muzyka',
+    'filmy',
+    'kody i doladowania',
+    'rowery i akcesoria',
+    'wedkarstwo',
+    'skating, slackline',
+    'antyki i sztuka',
+    'rekodzielo',
+    'teatr',
+    'zabawki'
+]
+
+walentynki = [
+    'perfumy',
+    'teatr',
+    'klasyka'
+    'ona'
+]
+
+dzien_kobiet_chlopaka = [
+    'perfumy',
+    'teatr',
+    'klasyka'
+    'ona',
+    'gry',
+    'ksiazki i komiksy',
+    'muzyka',
+    'teatr',
+    'perfumy',
+    'ona'
+]
+
+dzien_dziadka_babci_mamy_taty = [
+    'agd male',
+    'wyposazenie',
+    'narzedzia',
+    'perfumy',
+    'urzadzenia medyczne',
+    'medycyna naturalna',
+    'dermokosmetyki',
+    'suplementy diety',
+    'leki bez recepty'
+
+
+]
+
+dzien_dziecka = [
+    'konsole i automaty',
+    'zabawki',
+    'zabawki ogrodowe'
+    'książki i komiksy',
+    'gry',
+    'instrumenty',
+    'kody i doładowania',
+    'muzyka',
+    'filmy',
+    'rowery i akcesoria',
+    'skating, slackline'
+]
+
+slub = [
+    'telefony i akcesoria',
+    'komputery',
+    'konsole i automaty',
+    'fotografia',
+    'zabawki',
+    'zabawki ogrodowe',
+    'pielęgnacja',
+    'manicure i pedicure',
+    'makijaż',
+    'perfumy',
+    'książki i komiksy',
+    'gry',
+    'instrumenty',
+    'kody i doładowania',
+    'muzyka',
+    'filmy',
+    'rowery i akcesoria',
+    'turystyka',
+    'siłownia i fitness',
+    'wędkarstwo',
+    'skating, slackline',
+    'militaria'
+]
+
+komunia = [
+    'telefony i akcesoria',
+    'komputery',
+    'konsole i automaty',
+    'fotografia',
+    'popularne',
+    'perfumy',
+    'książki i komiksy',
+    'instrumenty',
+    'rowery i akcesoria',
+]
